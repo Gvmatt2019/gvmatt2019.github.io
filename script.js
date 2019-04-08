f1af9861f522de20f2152ee0a7f73860e624bc24
Gvmatt2019 <48104961+Gvmatt2019@users.noreply.github.com>
2019-04-08 12:50:04 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+<script type = "text/javascript">
 // 1. Create the button
 var button = document.createElement("button");
 button.innerHTML = "Do Something";
@@ -10,3 +11,4 @@ body.appendChild(button);
 button.addEventListener ("click", function() {
   alert("did something");
 });
+</script>
