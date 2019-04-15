2b76833b57d98dd5670ce739e9bd330513a17ef8
Gvmatt2019 <48104961+Gvmatt2019@users.noreply.github.com>
2019-04-15 13:01:06 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-<button type="button" onclick="myFunction()">Try it</button>
+var currentTime = new Date();
+var hours = currentTime.getHours();
+var minutes = currentTime.getMinutes();
+var seconds = currentTime.getSeconds();
+            var suffix = "AM";
+            if (minutes < 10)
+                minutes = "0" + minutes;
+            if (hours >= 12) {
+                suffix = "PM";
+                hours = hours - 12;
+            }
+            if (hours == 0) {
+                hours = 12;
+            }
+document.write("<b><div id='d1'>" + hours + ":" +  minutes + ":" +  seconds + " " + suffix + "</div></b>");
+ if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+  return i;
